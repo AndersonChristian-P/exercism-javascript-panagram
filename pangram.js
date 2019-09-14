933b2dfc019731fda36c6e07777d730cb6722221
Christian Anderson <ca96187@gmail.com>
2019-09-14 16:05:31 -0600

    did not pass all tests need to understand how to deal with mixed case and non alphabet characters

--- pangram.js
+++ pangram.js
@@ -1,10 +1,24 @@
-//
-// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+import { arrowFunctionExpression } from "@babel/types";
 
-export const isPangram = () => {
-  throw new Error("Remove this statement and implement this function");
+export const isPangram = str => {
+  let strToArr = str.split("")
+
+  let arr2 = []
+
+  for (let i = 0; i < strToArr.length; i++) {
+    if (!arr2.includes(strToArr[i])) {
+      arr2.push(strToArr[i])
+    }
+  }
+
+  if (arr2.length === 26) {
+    return true
+  } else {
+    return false
+  }
+
 };
 
-
+// step 1 - define the alphabet
+// step 2 - loop through the alphabet, if the letter does not exist in arr2 push it into arr2
+// step 3 - if arr2 equals 26 return true
